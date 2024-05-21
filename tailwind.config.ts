a916841042bf7a647cfea81f38b7890a28646d77
verdes ben <verdesb.devacc@gmail.com>
2024-05-21 11:50:10 +0200

    fix tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,8 +12,8 @@ const {
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ['class'],
-  // content: ["app/**/*.{ts,tsx}", "components/**/*.{ts,tsx}"],
-  content: [ 'src/**/*.{ts,tsx}'],
+
+  content: ['src/**/*.{ts,tsx}'],
   theme: {
     container: {
       center: true,
